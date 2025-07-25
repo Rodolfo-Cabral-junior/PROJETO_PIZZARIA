458130d3cc2ae0943a2cc20e1c56c84df8023d39
Rodolfo-Cabral-junior <rodolfocabra4@gmail.com>
2025-07-25 16:52:11 -0300

    animações 2

--- _scripts/script.js
+++ _scripts/script.js
@@ -65,15 +65,69 @@ $('ion-icon#youtube').click(function () {
      /*-------------------*/
     /*CÓDIGOS DE ANIMAÇÃO*/
    /*-------------------*/
-  $('section#domingo-perfeito').waypoint(function(direcao ) {
+   $('div#testemunhos ul:eq(0)').css('opacity ,0')
+   $('div#testemunhos ul:eq(1)').css('opacity ,0')
+   $('div#testemunhos ul:eq(2)').css('opacity ,0')
+   
+   $('section#domingo-perfeito').waypoint(function(direcao ) {
       if (direcao == "down") {
-          console.log('descendo!!!');
+          $('div.folha1').addClass('animate__animated animate__fadeInUp ');
+          $('div.folha3').addClass('animate__animated animate__fadeInUp ');
+          $('div.camarao').addClass('animate__animated animate__fadeInUp  animate__slow');
+          $('div.tomate').addClass('animate__animated animate__fadeInUp   anomate__slow ');
+          $('div.queijo').addClass('animate__animated animate__fadeInUp  animate__slower');
+          $('div.folha2').addClass('animate__animated animate__fadeInUp  animate__slower');
+         
         }
         if (direcao == "up") {
-            console.log('subindo!!!');
+          $('div.folha1').removeClass('animate__animated animate__fadeInUp ');
+          $('div.folha3').removeClass('animate__animated animate__fadeInUp ');
+          $('div.camarao').removeClass('animate__animated animate__fadeInUp  animate__slow');
+          $('div.tomate').removeClass('animate__animated animate__fadeInUp   anomate__slow ');
+          $('div.queijo').removeClass('animate__animated animate__fadeInUp  animate__slower');
+          $('div.folha2').removeClass('animate__animated animate__fadeInUp  animate__slower');
         }
         
   },{
-     offset:'100px;'
+     offset:'450px;'
   })
-  
+  $('section#pizza').waypoint(function(direcao){
+       if (direcao == 'down') {
+        $('section#pizza p').addClass('animate__animated animate__fadeInUp');
+        $('section#pizza div.pizza-opcoes').addClass('animate__animated  animate__fadeInUp   animate__slow');
+        
+       }
+       if (direcao == 'up') {
+        $('section#pizza p').removeClass('animate__animated animate__fadeInUp');
+        $('section#pizza div.pizza-opcoes').removeClass('animate__animated  animate__fadeInUp   animate__slow');
+       }
+  },{
+    offset:'450px;'
+})
+ $('section#testemunha').waypoint(function (direcao){
+     if (direcao == 'down'){
+          $('div#testemunhos ul:eq(0)').addClass('animate__animated animate__fadeInUp animate__slower');
+          $('div#testemunhos ul:eq(1)').addClass('animate__animated animate__fadeInUp animate__slowe');
+          $('div#testemunhos ul:eq(2)').addClass('animate__animated animate__fadeInUp animate__slower');
+     }
+     if (direcao == 'up'){
+          $('div#testemunhos ul:eq(0)').removeClass('animate__animated animate__fadeInUp animate__slower');
+          $('div#testemunhos ul:eq(1)').removeClass('animate__animated animate__fadeInUp animate__slowe');
+          $('div#testemunhos ul:eq(2)').removeClass('animate__animated animate__fadeInUp animate__slower');
+     }
+ },{
+   offset:'450px;' 
+ })
+ $('div#transicao-testemunha-plano').waypoint(function (direcao) {
+    if (direcao =='down' ) {
+         $('div#decoracao1').addClass('animate__animated animate__fadeInLeft animate__slow');
+         $('div#decoracao2').addClass('animate__animated animate__fadeIn animate__slow');
+         $('div#decoracao3').addClass('animate__animated animate__fadeInRight animate__slow');
+    }else{
+        $('div#decoracao1').removeClass('animate__animated animate__fadeInLeft animate__slow');
+        $('div#decoracao2').removeClass('animate__animated animate__fadeIn animate__slow');
+        $('div#decoracao3').removeClass('animate__animated animate__fadeInRight animate__slow');
+    }
+ },{
+  offset:'700px;'   
+ });
